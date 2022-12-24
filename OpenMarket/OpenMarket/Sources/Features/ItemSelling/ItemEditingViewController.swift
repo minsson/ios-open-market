7efa726cc8ac137640068f451987dcbf753e8fa1
minsson <mskaang@gmail.com>
2022-12-25 00:17:22 +0900

    feat: "ItemEditingViewController" 상품정보를 수정하는 기능 추가
    - "ItemEditingViewController"
        - setupRootView() 메서드를 통해 기본적인 기능 구현
        - 상품등록 뷰와 다르게 이미지를 추가, 수정, 삭제할 수 없도록 구현
        - 완료 버튼을 누르면 뷰의 각 컴포넌트들로부터 상품 정보를 취합해 서버로 Patch 요청

--- OpenMarket/OpenMarket/Sources/Features/ItemSelling/ItemEditingViewController.swift
+++ OpenMarket/OpenMarket/Sources/Features/ItemSelling/ItemEditingViewController.swift
@@ -9,27 +9,51 @@ import UIKit
 
 final class ItemEditingViewController: ItemSellingViewController {
     
+    // MARK: - Properties
+    
     private var itemDetail: ItemDetail?
+    
+    // MARK: - UI Components
+    
     private var imageViews: [UIImageView]?
     private var images: [UIImage]?
     
+    // MARK: - Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupRootView()
+        setupUIComponents()
+        configureDoneButton()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        setupUIComponents()
-        addingPhotoButtonImageView.isHidden = true
-    }
+}
+
+// MARK: - Actions
+
+extension ItemEditingViewController {
     
     func receiveData(_ itemDetail: ItemDetail?, _ images: [UIImage]) {
         self.itemDetail = itemDetail
         self.images = images
     }
     
-    private func setupUIComponents() {
+}
+
+// MARK: - Private Actions
+
+private extension ItemEditingViewController {
+    
+    // MARK: - Actions for UI
+    
+    func setupRootView() {
+        view.backgroundColor = .systemBackground
+        viewTitleLabel.text = "상품수정"
+        addingPhotoButtonImageView.isHidden = true
+    }
+    
+    func setupUIComponents() {
         guard let itemDetail = itemDetail else {
             return
         }
@@ -59,6 +83,67 @@ final class ItemEditingViewController: ItemSellingViewController {
             imageView.clipsToBounds = true
             photoStackView.addArrangedSubview(imageView)
         })
+    }
+    
+    // MARK: - Actions for Done Button
+
+    func configureDoneButton() {
+        doneButton.addTarget(self, action: #selector(doneButtonDidTap), for: .touchUpInside)
+    }
+    
+    @objc func doneButtonDidTap() {
+        pushDataToServer(data: assembleInputData())
+        dismiss(animated: true)
+    }
+    
+    func assembleInputData() -> Data? {
+        guard let name = nameTextField.text,
+              let priceText = priceTextField.text,
+              let price = Double(priceText),
+              let discountedPriceText = discountedPriceTextField.text,
+              let stockText = stockTextField.text,
+              let descriptionText = textView.text else {
+            return nil
+        }
+
+        let discountedPrice = Double(discountedPriceText) ?? 0
+        let stock = Int(stockText) ?? 0
+        let currency = currencySegmentedControl.selectedSegmentIndex == 0 ? Currency.krw : Currency.usd
+
+        let requestItem = Item(
+            name: name,
+            price: price,
+            discountedPrice: discountedPrice,
+            currency: currency.rawValue,
+            stock: stock,
+            description: descriptionText
+        )
+
+        do {
+            let requestItemData = try JSONEncoder().encode(requestItem)
+            return requestItemData
+        } catch {
+            return nil
+        }
+    }
+
+    func pushDataToServer(data: Data?) {
+        guard let itemDetail = itemDetail else {
+            return
+        }
+
+        guard let request = API.EditItem(productID: String(itemDetail.id), with: data).urlRequest else {
+            return
+        }
+        
+        // TODO: 네트워킹 방법 변경
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+        }.resume()
+
     }
     
 }
